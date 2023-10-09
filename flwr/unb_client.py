c43c8150cc392dc36906c1a9ba20af4cbd88abeb
rod_moreira <rodrigo.moreira206@gmail.com>
2023-10-09 00:46:22 +0000

    Changing dataset..

--- flwr/unb_client.py
+++ flwr/unb_client.py
@@ -70,7 +70,7 @@ class LSTMModel(nn.Module):
         self.hidden_layer = nn.Linear(hidden_size, hidden_size)
         self.output_layer = nn.Linear(hidden_size, output_size)
         self.activation = nn.ReLU()
-        self.dropout = nn.Dropout(0.2)
+        self.dropout = nn.Dropout(0.3)
 
     def forward(self, x):
         x = self.activation(self.input_layer(x))
