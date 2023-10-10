7210d20f4159a8d3130399c8fbcff1523a3c9782
rod_moreira <rodrigo.moreira206@gmail.com>
2023-10-10 11:22:07 +0000

    Changing dataset..

--- flwr/experiment1_unb.sh
+++ flwr/experiment1_unb.sh
@@ -4,14 +4,14 @@
 mkdir -p ./results/cic-unb/logs
 
 # Loop para executar o script 10 vezes
-for i in {1..10}; do
+for i in {2..10}; do
   log_file="./results/cic-unb/logs/log_$i.txt"  # Nome do arquivo de log com caminho
 
   # Iniciar o servidor e redirecionar a saída para o arquivo de log
   python3 unb_server.py --world_size 4 --dataset_id 1 --lr 0.001 &> "$log_file" &
   server_pid=$!  # Salvar o PID do servidor
 
-  sleep 3  # Aguardar um pouco antes de iniciar os clientes
+  sleep 10  # Aguardar um pouco antes de iniciar os clientes
 
   # Iniciar os clientes e redirecionar a saída para o mesmo arquivo de log
   python3 unb_client.py --dataset_id 3 --epochs 10 --batch_size 32 --lr 0.001 --optim SGD >> "$log_file" &
@@ -20,5 +20,7 @@ for i in {1..10}; do
 
   # Esperar que o servidor e os clientes terminem antes de prosseguir
   wait "$server_pid"
-  wait
+  ./clear_unb.sh
+  echo "FIM Primeiro Ciclo"
+  sleep 60
 done
