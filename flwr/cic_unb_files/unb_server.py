d2408c78d67ce67b14121f162d24d146cef842b3
rod_moreira <rodrigo.moreira206@gmail.com>
2023-11-04 13:14:52 +0000

    2nd round of experiments

--- flwr/cic_unb_files/unb_server.py
+++ flwr/cic_unb_files/unb_server.py
@@ -361,7 +361,7 @@ strategy = fl.server.strategy.FedAvg(
 # Start Flower server
 fl.server.start_server(
     server_address="0.0.0.0:8080",
-    config=fl.server.ServerConfig(num_rounds=2),
+    config=fl.server.ServerConfig(num_rounds=4),
     strategy=strategy
 )
 
